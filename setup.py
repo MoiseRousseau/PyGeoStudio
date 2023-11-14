289eb93630b449b4f7b92538510247e871fceaf1
M. Rousseau <rousseau.moise@gmail.com>
2023-11-14 10:07:08 -0500

    Update README

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='PyGeoStudio', 
-    version='0.1.0',  # Required
+    version='0.2.0',  # Required
     description='Python library allowing reading/writing GeoStudio .gsz files',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -17,18 +17,12 @@ setup(
     author='Moise Rousseau',  # Optional
     author_email='rousseau.moise@gmail.com', 
     classifiers=[ 
-        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        "Programming Language :: Python :: 3.10",
     ],
-    keywords='topology optimization, mechanics, numerical simulation', 
+    keywords='Geotechnics, Numerical Simulation, Hydrogeology, Optimization', 
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.6, <4',
@@ -43,6 +37,7 @@ setup(
     project_urls={ 
         'Bug Reports': 'https://github.com/MoiseRousseau/PyGeoStudio/issues',
         'Source': 'https://github.com/MoiseRousseau/PyGeoStudio/',
+        'Documentation': 'https://pygeostudio.readthedocs.io/',
     },
 )
 
