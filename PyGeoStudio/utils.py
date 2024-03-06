c886b18595423b8b35c0203a126b3979c53f744b
M. Rousseau <rousseau.moise@gmail.com>
2024-03-05 21:26:13 -0500

    Use WindowsPurePath when running simulation and add optional check of results.

--- PyGeoStudio/utils.py
+++ PyGeoStudio/utils.py
@@ -35,7 +35,7 @@ def defineGeoStudioLauncher(path):
   testLauncher(s)
   return
 
-def run(geofile, analyses_to_solve=None, shell=True):
+def run(geofile, analyses_to_solve=None, shell=True, check_output=True):
   """
   Call GeoStudio solver to run the analyses defined in the GeoStudio file
   
@@ -45,9 +45,11 @@ def run(geofile, analyses_to_solve=None, shell=True):
   :type analyses_to_solve: list of PyGeoStudio.Analysis object
   :param shell: Show the console (optional, ``True`` by default)
   :type shell: bool
+  :param check_output: Check if GeoStudio solver successfully solved the analysis
+  :type check_output: bool
   """
   if isinstance(geofile, PyGeoStudio.GeoStudioFile):
-    geofile = geofile.f_src
+    geofile = pathlib.PureWindowsPath(geofile.f_src)
   if analyses_to_solve is not None:
     analyses_to_solve_name = [x["Name"] for x in analyses_to_solve]
   else:
@@ -55,18 +57,24 @@ def run(geofile, analyses_to_solve=None, shell=True):
   cmd = [geopath + "\\Bin\\GeoCmd.exe", geofile] + analyses_to_solve_name + ["/solve"]
   print("#################################")
   print("Calling GeoStudio solver")
-  ret_code = subprocess.run(cmd, shell=shell)
-  print(ret_code)
+  cmd_out = subprocess.run(cmd, shell=shell)
+  if check_output and cmd_out.returncode:
+    message = "Call to GeoStudio ends up with no zero status code, which mean a failure of the simulation. Check GeoStudio output above for more detailed information."
+    raise RuntimeError(message)
   print("#################################")
-  return ret_code
+  return cmd_out.returncode
 
 def testLauncher(path=None):
   if path is None: path = geopath
   cmd = [path + "\\Bin\\GeoCmd.exe", "/?"]
-  output = subprocess.run(cmd, check=False, stdout=subprocess.PIPE).stdout.decode()
+  error_message = "Can't find GeoStudio executables with the path provided. Please correct the path and redefine it with defineGeoStudioLauncher() method."
+  try:
+    output = subprocess.run(cmd, check=False, stdout=subprocess.PIPE).stdout.decode()
+  except:
+    raise ValueError(error_message)
   if "GeoCmd" in output and "Copyright" in output:
     print("GeoStudio version:" + output.split("\n")[0].split("version")[-1])
     print("Successfully tested GeoStudio executable")
   else:
-    raise ValueError("Can't find GeoStudio executables with the path provided. Please correct the path and redefine it with defineGeoStudioLauncher() method")
+    raise ValueError(error_message)
   return
